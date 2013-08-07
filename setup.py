28eb4deabe559d51a15c4e5c17324ce5d692a95e
Борисов Кирилл <borisov@med.bars-open.ru>
2013-08-07 14:06:06 +0400

    Первый набросок, подхватывающий behave-тесты.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='wellbehaved',
+    packages=['wellbehaved'],
+    version='0.0.1',
+    description='Simple Django Test Runner for the Behave BDD module with i18n support',
+    author='Kirill Borisov',
+    author_email='borisov@bars-open.ru',
+    url='http://src.bars-open.ru/py/WebEdu/tools/wellbehaved',
+    classifiers=[
+        'Programming Language :: Python :: 2.6',
+        'Framework :: Django',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Testing',
+    ],
+    install_requires=[
+        'Django>=1.4',
+        'behave',
+    ]
+)
---
+++ wellbehaved/runner.py
@@ -0,0 +1,60 @@
+#coding: utf-8
+
+import os
+import sys
+import unittest
+
+from django.db.models import get_app
+from django.test import TestCase
+from django.test.simple import DjangoTestSuiteRunner, reorder_suite
+from django.conf import settings
+
+from behave.runner import Runner
+from behave.configuration import Configuration
+
+
+class DjangoBDDTestCase(TestCase):
+    def __init__(self, *args, **kwargs):
+        self.behaviour_dir = kwargs.pop('behaviour_dir')
+
+        super(DjangoBDDTestCase, self).__init__(**kwargs)
+
+    def setUp(self):
+        # Так как behave пытается обрабатывать аргументы командной строки
+        # и это пока что (<=1.2.3) не получается отключить, то приходится
+        # применять жуткий хак с подменой командной строки
+        old_argv = sys.argv
+        sys.argv = sys.argv[:2]
+        self.behave_configuration = Configuration()
+        sys.argv = old_argv
+
+        self.behave_configuration.paths = [self.behaviour_dir]
+        self.behave_configuration.format = ['pretty']        
+        self.behave_configuration.stdout_capture = False
+        self.behave_configuration.stderr_capture = False
+
+        # Пробуем получить язык, на котором написаны сценарии для behave
+        self.behave_configuration.lang = getattr(settings, 'WELLBEHAVED_LANG', 'ru')
+
+    def runTest(self, result=None):
+        runner = Runner(self.behave_configuration)
+
+        assert runner.run(), 'BDD test has failed.'
+
+
+class DjangoBDDTestSuiteRunner(DjangoTestSuiteRunner):
+
+    def build_suite(self, test_apps, extra_tests=None, **kwargs):
+        suite = unittest.TestSuite()
+        std_django_suite = DjangoTestSuiteRunner().build_suite(test_apps, **kwargs)
+        suite.addTest(std_django_suite)
+
+        for app_name in test_apps:
+            app = get_app(app_name)
+
+            module_path = os.path.dirname(app.__file__)
+            behaviour_path = os.path.join(module_path, 'features')
+            if os.path.isdir(behaviour_path):
+                suite.addTest(DjangoBDDTestCase(behaviour_dir=behaviour_path))
+
+        return reorder_suite(suite, (TestCase,))

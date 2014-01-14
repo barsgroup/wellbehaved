bd4c1107ba69ee8d79ead5aab5aa4f69cfdd7a62
Борисов Кирилл <borisov@bars-open.ru>
2014-01-14 16:24:46 +0400

    Добавлена MIT лицензия.

--- wellbehaved/utils.py
+++ wellbehaved/utils.py
@@ -1,53 +1,26 @@
 #coding: utf-8
-
-
-class HookDictWrapper(dict):
-    u'''
-    Класс, "прозрачно" перехватывающий установку обработчиков шагов
-    тестирования в environment.py. В случае, если мы тоже обрабатываем
-    этот шаг, то мы оборачиваем переданный обработчик таким образом,
-    что наш код всегда испольняется первым.
-
-    На данный момент используется для гарантирования срабатывания кода
-    частичного отката изменения после прохождения шага сценария.
-    '''
-
-    def set_wrappers(self, wrapped):
-        self.wrapped = wrapped
-        # По умолчанию устанавливаем свои обработчики шагов
-        for hook, handler in self.wrapped.items():
-            super(HookDictWrapper, self).__setitem__(hook, handler)
-
-    def __init__(self, wrapped=None):
-        self.set_wrappers(wrapped or {})
-
-    def __setitem__(self, hook, handler):
-        '''
-        Перехватчик установки обработчика очередного шага пользовательским
-        кодом. Устанавливаемый обработчик будет всегда выполнятся после нашего.
-
-        @param hook    Код события ([after|before]_[feature|step|...])
-        @param handler Обработчик события из environment.py
-        '''
-
-        def wrap_hook(name, original_hook):
-            def wrapper(*args, **kwargs):
-                self.wrapped[name](*args, **kwargs)
-                original_hook(*args, **kwargs)
-            return wrapper
-
-        if hook in self.wrapped:
-            super(HookDictWrapper, self).__setitem__(hook,
-                                                     wrap_hook(hook, handler))
-        else:
-            super(HookDictWrapper, self).__setitem__(hook, handler)
-
+u'''
+Вспомогательный класс подсистемы плагинов.
+'''
 
 class StackedHookDictWrapper(dict):
+    u'''
+    Унаследованный от *dict* класс, "прозрачно" перехватываюиbщий установку
+    обработчиков шагов тестирования в environment.py.
+
+    Каждый перехваченный обработчик добавляется в стэк, связанный
+    с этим конкретным этапом тестирования и при каждом .
+    '''
     def __init__(self):
         self.hook_registry = {}
 
     def __setitem__(self, hook, handler):
+        u'''
+        Перехватчик установки обработчиков из environment.py.
+
+        :param hook: Идентификатор обработчика (см. документацию behave).
+        :param handler: Функция, обрабатывающая определенный шаг тестирования.
+        '''
         def wrapper(*args, **kwargs):
             fnlist = self.hook_registry[hook]
             for fn in fnlist:

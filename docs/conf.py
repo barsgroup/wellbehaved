e02cf59271af9652cd9128cc438154479cd6eddf
Борисов Кирилл <borisov@bars-open.ru>
2014-01-14 15:32:22 +0400

    Первый вариант документации.

---
+++ docs/conf.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+
+source_suffix = '.rst'
+
+
+master_doc = 'index'
+
+# General information about the project.
+project = u'wellbehaved'
+project_id = u'wellbehaved'
+company = 'BARS Group'
+copyright = u'2013-%s, %s' % (datetime.now().year, company)
+
+version = '1.2.0'
+release = '1.2.0'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build']
+
+pygments_style = 'sphinx'
+
+html_theme = 'default'
+
+html_static_path = ['_static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'wellbehaveddoc'
+
+autodoc_default_flags = ['members', 'undoc-members']
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. wellbehaved documentation master file, created by
+   sphinx-quickstart on Tue Jan 14 13:56:46 2014.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to wellbehaved's documentation!
+=======================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   BDD.rst
+   README.rst
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
